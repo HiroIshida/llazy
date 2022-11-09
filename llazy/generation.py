bbfd5ff36eab2b70c00ea057e27f861c23a8851d
HiroIshida <spitfire.docomo@gmail.com>
2022-11-09 19:10:16 +0900

    Add genration task class

---
+++ llazy/generation.py
@@ -0,0 +1,46 @@
+import uuid
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from multiprocessing import Process
+from pathlib import Path
+from typing import Generic
+
+import numpy as np
+import tqdm
+
+from llazy.dataset import ChunkT
+
+
+@dataclass
+class DataGenerationTaskArg:
+    process_idx: int  # is not pid
+    number: int
+    show_process_bar: bool
+    base_path: Path
+
+
+class DataGenerationTask(ABC, Process, Generic[ChunkT]):
+    arg: DataGenerationTaskArg
+
+    def __init__(self, arg: DataGenerationTaskArg):
+        self.arg = arg
+        super().__init__()
+        self.post_init_hook()
+
+    @abstractmethod
+    def post_init_hook(self) -> None:
+        pass
+
+    @abstractmethod
+    def generate_single_data(self) -> ChunkT:
+        pass
+
+    def run(self) -> None:
+        np.random.seed(self.arg.process_idx)
+        disable_tqdm = not self.arg.show_process_bar
+
+        for _ in tqdm.tqdm(range(self.arg.number), disable=disable_tqdm):
+            chunk = self.generate_single_data()
+            name = str(uuid.uuid4()) + ".pkl"
+            file_path = self.arg.base_path / name
+            chunk.dump(file_path)
